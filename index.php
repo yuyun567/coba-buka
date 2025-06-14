4f40343938c916605de1e3eced3f23f306fd135e
yuyun567 <zahraaullia09@gmail.com>
2025-06-14 12:51:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+<form action='log.php' method='POST'>
+ <input type="text" 
+           name="username" 
+           placeholder="username"
+ <input type="password" 
+          name="password" 
+          placeholder="password"
 
+ <input type='submit'/>    
+</form 
